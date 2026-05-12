18fe3e485c40f54979a9c19fecedde899d95097f
radisa-bogdanovic <tictac992@gmail.com>
2026-05-12 18:52:14 +0200

    initial commit

---
+++ src/app/header/header.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  imports: [],
+  templateUrl: './header.html',
+})
+export class Header {
+  protected readonly title = 'Ovo je title';
+}
